9af1e97f41c1cc850159632239cabbf3979f0bc2
Jhônata Tavares <jhonata.tavares@faculdadegran.edu.br>
2025-07-07 23:25:02 -0300

    mysql2-addiction-and-bugs-fixed

--- Blog/app.js
+++ Blog/app.js
@@ -1,18 +1,19 @@
 import express from 'express';
-import { ExpressHandlebars } from 'express-handlebars';
+import expressHandlebars from 'express-handlebars';
 import { Sequelize } from 'sequelize';
 
 const app = express();
 
 // Configurando o Handlebars como motor de visualização
-    app.engine('handlebars', new ExpressHandlebars({defaultLayout: 'main'}));
-    app.views('views', 'handlebars')
+app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }));
+app.set('views', './views');
+app.set('view engine', 'handlebars');
 
 // Estabelecendo a conexão com o banco de dados MySQL.
-    const sequelize = new Sequelize('blog', 'root', '@Jhonata123', {
-        host: 'localhost',
-        dialect: 'mysql'
-    });
+const sequelize = new Sequelize('blog', 'root', '@Jhonata123', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 
 app.listen(8081, function(){
     console.log('Servidor rodando com sucesso na porta 8081');
