d41a8bce253c223c088a85e7e5296d811efa6661
Jhônata <jhonatasos206@gmail.com>
2025-07-14 19:55:14 -0300

    Configuration-of-app.js-file.and-handlebars

--- Blog/app.js
+++ Blog/app.js
@@ -1,18 +1,29 @@
+// Importando as dependências necessárias, juntamente do módulo para manipulação de caminhos e URLs, o PATH
 import express from 'express';
 import expressHandlebars from 'express-handlebars';
 import { Sequelize } from 'sequelize';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+// Definindo as variáveis necessárias para o funcionamento da API
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 // Configurando o Handlebars como motor de visualização
 app.engine('handlebars', expressHandlebars.engine({ defaultLayout: 'main' }));
-app.set('views', './views');
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'handlebars');
 
 // Estabelecendo a conexão com o banco de dados MySQL.
 const sequelize = new Sequelize('blog', 'root', '@Jhonata123', {
     host: 'localhost',
     dialect: 'mysql'
+});
+
+// Rotas
+app.get('/cad', function(req, res){
+    res.render('formulario')
 });
 
 app.listen(8081, function(){
